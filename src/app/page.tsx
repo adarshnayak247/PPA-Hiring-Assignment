f74b9153170421dd65c8e41d11d683e44b336f4c
Adarsh <97530655+adarshnayak247@users.noreply.github.com>
2024-07-07 22:58:29 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 
 
-import AchievementSection from "../components/common/Achievement";
-import CalenderSection from "../components/common/Calender";
-import Client from "../components/common/Client";
-import CommunitySection from "../components/common/Community";
-import CustomerSection from "../components/common/Customer";
-import Footer from "../components/common/footer";
-import Frame16 from "../components/common/Frame16";
-import Hero from "../components/common/Hero";
-import { Navbar } from "../components/common/Navbar";
-import UnlockSection from "../components/common/Unlock";
-import UpdatesSection from "../components/common/Updates";
+import AchievementSection from "@/components/common/Achievement";
+import CalenderSection from "@/components/common/Calender";
+import Client from "@/components/common/Client";
+import CommunitySection from "@/components/common/Community";
+import CustomerSection from "@/components/common/Customer";
+import Footer from "@/components/common/footer";
+import Frame16 from "@/components/common/Frame16";
+import Hero from "@/components/common/Hero";
+import { Navbar } from "@/components/common/Navbar";
+import UnlockSection from "@/components/common/Unlock";
+import UpdatesSection from "@/components/common/Updates";
 
 
 import Image from "next/image";

20ccbc3eee3a9c3bbcd8d8332c75123d7bb9f15b
adarshnayak247 <adarshnayak247@gmail.com>
2024-07-07 21:52:50 +0530

    Added Achievementsection,calende section, count up animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 
 
+import AchievementSection from "@/components/common/Achievement";
+import CalenderSection from "@/components/common/Calender";
 import Client from "@/components/common/Client";
 import CommunitySection from "@/components/common/Community";
 import Hero from "@/components/common/Hero";
 import { Navbar } from "@/components/common/Navbar";
+import UnlockSection from "@/components/common/Unlock";
 
 
 import Image from "next/image";
@@ -15,6 +18,9 @@ export default function Home() {
      <Hero/>
      <Client/>
      <CommunitySection/>
+     <UnlockSection/>
+     <AchievementSection/>
+     <CalenderSection/>
     </>
   );
 }
